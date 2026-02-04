7372c5e2ee093538505e4afb036a6e036702b697
Roman <joromeax@gmail.com>
2026-02-04 19:39:03 +0200

    Initial Commit

---
+++ testForBoosteight/testForBoosteightApp.swift
@@ -0,0 +1,17 @@
+//
+//  testForBoosteightApp.swift
+//  testForBoosteight
+//
+//  Created by Roman on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct testForBoosteightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
